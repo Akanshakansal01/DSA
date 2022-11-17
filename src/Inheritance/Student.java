c162d491fc1b969628b37198216128e431961791
Akanshakansal01 <akansha.kansal_cs20@gla.ac.in>
2022-11-17 15:06:17 +0530

    recurrence

---
+++ src/Inheritance/Student.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Student {
+    String name = "Kunj";
+    int age = 9;
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.age;
+    }
+}
