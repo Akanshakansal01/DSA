33b1df1b9a2aee5cfc0cbc8052efec78f8d77473
Akanshakansal01 <akanshakansal025@gmail.com>
2023-01-12 09:55:44 +0530

    Tree

---
+++ src/Tree/Binary_Tree.java
@@ -0,0 +1,118 @@
+package Tree;
+
+import java.util.Scanner;
+
+public class Binary_Tree {
+    private class Node {
+        int val;
+        Node left;
+        Node right;
+    }
+
+    private Node root;
+    Scanner sc= new Scanner(System.in);
+
+    public Binary_Tree()
+    {
+        root=CreateTree();
+    }
+
+    private Node CreateTree()
+    {
+        int item = sc.nextInt();           // 10(root)
+        Node nn=new Node();
+        nn.val=item;
+        boolean hlc= sc.nextBoolean();       // hlc= has left child
+        if(hlc)
+        {
+            nn.left=CreateTree();
+        }
+        boolean hrc= sc.nextBoolean();       // hrc= has right child
+        if(hrc)
+        {
+            nn.right=CreateTree();
+        }
+        return  nn;
+    }
+
+    public void Display()
+    {
+        Display(root);
+    }
+
+    private void Display(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        String s = "";
+        s = "<--" +s+ node.val + "-->";
+        if(node.left !=null)
+        {
+            s=node.left.val +s;
+        }
+        else
+        {
+            s= "."+s;
+        }
+        if(node.right !=null)
+        {
+            s= s + node.right.val;
+        }
+        else
+        {
+            s= s + ".";
+        }
+        System.out.println(s);
+        Display(node.left);
+        Display(node.right);
+    }
+
+    public void PreOrder()
+    {
+        PreOrder(root);
+        System.out.println();
+    }
+    private void PreOrder(Node nn)
+    {
+        if (nn == null) {
+            return;
+        }
+        System.out.print(nn.val + " ");
+        PreOrder(nn.left);
+        PreOrder(nn.right);
+    }
+
+    public void PostOrder()
+    {
+        PostOrder(root);
+        System.out.println();
+    }
+    private void PostOrder(Node nn)
+    {
+        if (nn == null) {
+            return;
+        }
+        PostOrder(nn.left);
+        PostOrder(nn.right);
+        System.out.print(nn.val + " ");
+    }
+
+    public void InOrder()
+    {
+        InOrder(root);
+        System.out.println();
+    }
+    private void InOrder(Node nn)
+    {
+        if (nn == null) {
+            return;
+        }
+        InOrder(nn.left);
+        System.out.print(nn.val + " ");
+        InOrder(nn.right);
+    }
+
+
+}
---
+++ src/Tree/Binary_Tree_Client.java
@@ -0,0 +1,19 @@
+package Tree;
+
+public class Binary_Tree_Client {
+    public static void main(String[] args) {
+        Binary_Tree bt= new Binary_Tree();
+        bt.Display();
+        // 10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false
+//        20<--10-->30
+//        40<--20-->50
+//        .<--40-->.
+//        .<--50-->.
+//        .<--30-->60
+//         70<--60-->.
+//         .<--70-->.
+        bt.PreOrder();   //  10 20 40 50 30 60 70
+        bt.PostOrder();  //  40 50 20 70 60 30 10
+        bt.InOrder();   //   40 20 50 10 30 70 60
+    }
+}
