bda16c15830a0091f25a8b830c66434b5125b21e
Akanshakansal01 <akanshakansal025@gmail.com>
2023-01-19 13:43:33 +0530

    Binary Tree

---
+++ src/Tree/Diameter_Of_Binary_Tree.java
@@ -0,0 +1,36 @@
+package Tree;
+
+public class Diameter_Of_Binary_Tree {
+    public class TreeNode {
+        int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+            if(root==null)
+            {
+                return 0;
+            }
+            int leftDiameter= diameterOfBinaryTree(root.left);
+            int rightDiameter = diameterOfBinaryTree(root.right);
+            int sd=height(root.left)+height(root.right)+2;
+            return Math.max(leftDiameter,Math.max(rightDiameter,sd));
+        }
+        public int height(TreeNode root)
+        {
+            if(root==null)
+                return -1;
+            int leftHeight=height(root.left);
+            int rightHeight=height(root.right);
+            return Math.max(leftHeight,rightHeight)+1;
+        }
+    }
+}
