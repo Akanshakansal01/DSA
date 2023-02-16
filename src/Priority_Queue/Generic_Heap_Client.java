d3d8c81898f9535afdf4f8187f79a8829b9c10af
Akanshakansal01 <akanshakansal025@gmail.com>
2023-02-16 15:44:43 +0530

    Priority queue

--- src/Priority_Queue/Generic_Heap_Client.java
+++ src/Priority_Queue/Generic_Heap_Client.java
@@ -10,6 +10,7 @@ public class Generic_Heap_Client {
         gp.add(new Cars(34, 89, "Grey"));
         gp.add(new Cars(8907, 6, "Red"));
         gp.display();
-
+        System.out.println(gp.max());
+        gp.display();
     }
 }
