b10e54491bb2cf67daaf59560aca457bfef1e7c9
Akanshakansal01 <akanshakansal025@gmail.com>
2023-03-16 10:04:30 +0530

    graph

---
+++ src/Graph/Graph.java
@@ -0,0 +1,76 @@
+package Graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Graph {
+    private HashMap<Integer,HashMap<Integer,Integer>> map= new HashMap<>();
+    public Graph(int v)
+    {
+        for(int i=1;i<=v;i++)
+        {
+            map.put(i,new HashMap<>());
+        }
+    }
+
+    public void AddEdge(int v1, int v2, int cost)
+    {
+        map.get(v1).put(v2, cost);   // v1--> map get kra then put v2 and Cost  //  v1 --> v2
+        map.get(v2).put(v1, cost);  //  v2 --> v1
+    }
+
+    public boolean ContainsEdge(int v1, int v2)
+    {
+        return map.get(v1).containsKey(v2);
+    }
+
+    public boolean Containsvertex(int v1)
+    {
+        return map.containsKey(v1);
+    }
+
+    public int noofEdge() {
+        int sum = 0;
+        for (int key : map.keySet()) {
+            sum = sum + map.get(key).size();
+        }
+        return sum / 2;
+    }
+
+    public void removeEdge(int v1, int v2) {
+//        if (ContainsEdge(v1, v2)) {
+            map.get(v1).remove(v2);  // v1 --> v2
+            map.get(v2).remove(v1);  // v2 --> v1
+//        }
+    }
+
+    public void removevertex(int v1) {
+        for (int key : map.get(v1).keySet()) {
+            map.get(key).remove(v1);
+        }
+        map.remove(v1);
+    }
+    public void display()
+    {
+        for(int key: map.keySet()){
+            System.out.println(key+"-> "+map.get(key));
+        }
+    }
+    public boolean hashpath(int src, int des, HashSet<Integer> visited) {
+        if (src == des) {
+            return true;
+        }
+        visited.add(src);
+        for (int nbrs : map.get(src).keySet()) {
+            if (!visited.contains(nbrs)) {
+                boolean ans = hashpath(nbrs, des, visited);
+                if (ans) {
+                    return ans;
+                }
+            }
+        }
+        visited.remove(src);
+        return false;
+    }
+
+}
---
+++ src/Graph/Graph_Client.java
@@ -0,0 +1,36 @@
+package Graph;
+
+import java.util.HashSet;
+
+public class Graph_Client {
+    public static void main(String[] args) {
+        Graph g= new Graph(7);
+        g.AddEdge(1, 4, 6);
+        g.AddEdge(1, 2, 10);
+        g.AddEdge(2, 3, 7);
+        g.AddEdge(3, 4, 5);
+        g.AddEdge(4, 5, 1);
+        g.AddEdge(5, 6, 4);
+        g.AddEdge(7, 5, 2);
+        g.AddEdge(6, 7, 3);
+        g.display();
+//        1-> {2=10, 4=6}
+//        2-> {1=10, 3=7}
+//        3-> {2=7, 4=5}
+//        4-> {1=6, 3=5, 5=1}
+//        5-> {4=1, 6=4, 7=2}
+//        6-> {5=4, 7=3}
+//        7-> {5=2, 6=3}
+        g.removevertex(4);
+        System.out.println();
+        g.display();
+//        1-> {2=10}
+//        2-> {1=10, 3=7}
+//        3-> {2=7}
+//        5-> {6=4, 7=2}
+//        6-> {5=4, 7=3}
+//        7-> {5=2, 6=3}
+        System.out.println(g.hashpath(1, 6, new HashSet<>()));  // false
+//        g.printallpath(1,  6, new HashSet<>(), "");
+    }
+}
