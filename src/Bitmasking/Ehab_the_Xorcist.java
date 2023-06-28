71523233b1963de25b459cbc439037792b4ee38a
Akanshakansal01 <akanshakansal025@gmail.com>
2023-06-28 16:33:37 +0530

    bitmasking

---
+++ src/Bitmasking/Ehab_the_Xorcist.java
@@ -0,0 +1,40 @@
+//   codeforces ----->   1325  D. Ehab the Xorcist
+package Bitmasking;
+
+import java.util.Scanner;
+
+public class Ehab_the_Xorcist {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        long u=sc.nextLong();
+        long v=sc.nextLong();
+        if(u>v || (v-u)%2!=0){
+            System.out.println(-1);
+        }
+        else if(u==v){
+            if(v==0){
+                System.out.println(0);
+            }
+            else{
+                System.out.println(1);
+                System.out.println(v);
+            }
+        }
+        else{
+            long x=(v-u)/2;
+            if((u&x)==0){
+                System.out.println(2);
+                System.out.println(u+x +" "+x);
+            }
+            else{
+                System.out.println(3);
+                System.out.println(u +" "+x+" "+x);
+            }
+        }
+    }
+
+}
+
+// p+q = p^q +2(p&q)    ------>        v = u + 2(p&q)
+
+
